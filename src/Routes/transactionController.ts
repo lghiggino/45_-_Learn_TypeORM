0c9a8c3d0e629ee89ed5b8705817fa5428fe5151
lghiggino <lghiggino@gmail.com>
2021-10-21 10:31:37 -0300

    fix balance type transformation

--- src/Routes/transactionController.ts
+++ src/Routes/transactionController.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { Transaction } from "../Entities/Transaction"
+import { TransactionTypes } from "../Entities/Transaction"
 import { Client } from "../Entities/Client"
 const router = express.Router()
 
@@ -13,19 +14,30 @@ router.post("/api/client/:clientId/transaction", async (req, res) => {
 
     const { clientId } = req.params
 
+
     const client = await Client.findOne(parseInt(clientId))
-    if(!client){
-        res.json("Client not found")
+    if (!client) {
+        res.json({ message: "Client not found" })
+        return
     }
 
     const transaction = Transaction.create({
         amount,
-        type, 
+        type,
         client //passes the whole client
     })
 
     await transaction.save()
-    return res.json(transaction)
+
+    if (type === TransactionTypes.DEPOSIT) {
+        client.balance = +client.balance + amount
+    } else if (type === TransactionTypes.WITHDRAW && client.balance >= amount) {
+        client.balance = +client.balance - amount
+    } else {
+        return res.json({ message: "Unable to complete the transaction - funds unavailable" })
+    }
+    await client.save()
+    return res.json({ message: "Transaction concluded" })
 })
 
 export { router as transacitionController }
