7875842b684b940ec7b83049200c4f3a221a4b76
lghiggino <lghiggino@gmail.com>
2021-10-21 09:21:51 -0300

    passing the clientId in the req.params and treating error

--- src/Routes/transactionController.ts
+++ src/Routes/transactionController.ts
@@ -1,15 +1,22 @@
 import express from "express"
 import { Transaction } from "../Entities/Transaction"
+import { Client } from "../Entities/Client"
 const router = express.Router()
 
 router.get("/api/transaction", async (req, res) => { })
 
-router.post("/api/transaction", async (req, res) => {
+router.post("/api/client/:clientId/transaction", async (req, res) => {
     const {
         amount,
-        type,
-        client
+        type
     } = req.body
+
+    const { clientId } = req.params
+
+    const client = await Client.findOne(parseInt(clientId))
+    if(!client){
+        res.json("Client not found")
+    }
 
     const transaction = Transaction.create({
         amount,
