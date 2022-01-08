99126326a8ce18eeedc9ee101753d48cdd6e524b
lghiggino <lghiggino@gmail.com>
2022-01-08 09:11:35 -0300

    add type to @Column and new scripts to start project

--- withMySQL/readme.md
+++ withMySQL/readme.md
@@ -1,19 +1,14 @@
-#RCH-FLASH-API
-
+#Criando o container docker com mysql
 Passo a passo para criar a conexão
 - $ docker run --name withmysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -p 3306:3306 mysql:latest
-
-
 -- No DBeaver: Aba SSL > Use SSL > deixar os campos em branco
 
-
 Se o container já existir
-- $ docker run rchmysql
+- $ docker run withmysql
 
 
 ##Rodando:
-- $ yarn build - constroi o arquivo de rotas
-- $ yarn run dev - inicia o TSOA com o nodemon
+- $ yarn run dev2 - Faz o build e roda o nodemon com o app funcionando.
 
 
 ###Rotas:
--- withMySQL/src/entities/User.ts
+++ withMySQL/src/entities/User.ts
@@ -3,18 +3,19 @@ import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("user")
 export class User extends BaseEntity {
+
     @PrimaryGeneratedColumn()
     id!: number;
 
-    // @Column()
-    // email: string | undefined;
+    @Column({type: "text"})
+    email: string | undefined;
 
-    // @Column()
-    // name: string | undefined;
+    @Column({type: "text"})
+    name: string | undefined;
 
-    // @Column()
-    // status?: string | undefined;
+    @Column({type: "text"})
+    status?: string | undefined;
 
-    // @Column()
-    // phoneNumbers: string[] | undefined;
+    @Column("simple-array")
+    phoneNumbers: string[] | undefined;
 }
