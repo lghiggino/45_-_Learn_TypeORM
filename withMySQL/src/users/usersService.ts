ec51b2ea0dd8ab5653b70da732d323326c9a709c
lghiggino <lghiggino@gmail.com>
2022-01-09 09:18:16 -0300

    usersService

--- withMySQL/src/users/usersService.ts
+++ withMySQL/src/users/usersService.ts
@@ -1,8 +1,8 @@
 import { getRepository } from "typeorm";
 import { User } from "../entities/User";
 
-
 export type UserCreationParams = Pick<User, "email" | "name" | "phoneNumbers">
+
 
 export class UsersService {
     public async get(id: number): Promise<User> {
