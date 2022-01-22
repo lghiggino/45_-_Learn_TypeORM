e7bdeddd75557462eedfdd0bce39c5710bfd88f3
lghiggino <lghiggino@gmail.com>
2022-01-21 21:44:04 -0300

    createStudent and createContent migrations done

--- project-template-AL/src/migrations/1642811848572-CreateStudent.ts
+++ project-template-AL/src/migrations/1642811848572-CreateStudent.ts
@@ -1,11 +1,49 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateStudent1642811848572 implements MigrationInterface {
+export default class CreateStudent1642811848572 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'student',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isNullable: false,
+            isPrimary: true,
+            isUnique: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'key',
+            type: 'varchar',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'update_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'deleted_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('student');
+  }
 }
--- project-template-AL/src/migrations/1642811861097-CreateContent.ts
+++ project-template-AL/src/migrations/1642811861097-CreateContent.ts
@@ -1,11 +1,48 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateContent1642811861097 implements MigrationInterface {
+export default class CreateContent1642811861097 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'content',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isNullable: false,
+            isPrimary: true,
+            isUnique: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'descriptiom',
+            type: 'varchar',
+          },
+          {
+            name: 'linkContent',
+            type: 'varchar',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'update_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'deleted_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('content');
+  }
 }
