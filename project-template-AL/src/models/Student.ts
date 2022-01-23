e7eaf7b5dfbeb72ed146e7c04ec9f946edc84b5d
lghiggino <lghiggino@gmail.com>
2022-01-23 19:41:43 -0300

    student model

--- project-template-AL/src/models/Student.ts
+++ project-template-AL/src/models/Student.ts
@@ -1,7 +1,33 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export default class Student {
   @PrimaryGeneratedColumn('increment')
   id: number;
+
+  @Column({
+    length: 100,
+  })
+  name: string;
+
+  @Column({
+    length: 45,
+  })
+  key: string;
+
+  @CreateDateColumn({ name: 'created_At' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_At' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_At' })
+  deletedAt: Date;
 }
