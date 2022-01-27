1ff5cfbad5359c0e23186a7e3fa78d1ee2aa0fdf
lghiggino <lghiggino@gmail.com>
2022-01-27 12:20:19 -0300

    createNew Lesson + Student

---
+++ project-template-AL/src/database/migrations/1643296496126-CreateNewLesson.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateNewLesson1643296496126 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'lesson',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isNullable: false,
+            isPrimary: true,
+            isUnique: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_At',
+            type: 'timestamp',
+            default: 'null',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_At',
+            type: 'timestamp',
+            default: 'null',
+            isNullable: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('lesson');
+  }
+}
---
+++ project-template-AL/src/database/migrations/1643296512932-CreateNewStudent.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateNewStudent1643296512932 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'student',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isNullable: false,
+            isPrimary: true,
+            isUnique: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'key',
+            type: 'varchar',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_At',
+            type: 'timestamp',
+            default: 'null',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_At',
+            type: 'timestamp',
+            default: 'null',
+            isNullable: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('student');
+  }
+}
