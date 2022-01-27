ed279d33918dd6db2400eaa7bde02a89d6f74c7c
lghiggino <lghiggino@gmail.com>
2022-01-27 09:29:58 -0300

    Nova migration para realizar o bugfix

---
+++ project-template-AL/src/database/migrations/1643286280344-CreateNewClass.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateNewClass1643286280344 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'class',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isNullable: false,
+            isPrimary: true,
+            isUnique: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'duration',
+            type: 'integer',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_At',
+            type: 'timestamp',
+            default: 'null',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_At',
+            type: 'timestamp',
+            default: 'null',
+            isNullable: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('class');
+  }
+}
