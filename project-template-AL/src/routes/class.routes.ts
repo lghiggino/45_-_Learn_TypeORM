6b7ae7aae8642b39a6ada85e60e5ed7af503a620
lghiggino <lghiggino@gmail.com>
2022-01-27 12:38:52 -0300

    classRoute with params

--- project-template-AL/src/routes/class.routes.ts
+++ project-template-AL/src/routes/class.routes.ts
@@ -10,4 +10,11 @@ classRouter.post('/', async (req, res) => {
   return res.status(201).json(savedClass);
 });
 
+classRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const repo = getRepository(Class);
+  const classById = await repo.findOneOrFail(id);
+  return res.status(200).json(classById);
+});
+
 export default classRouter;
