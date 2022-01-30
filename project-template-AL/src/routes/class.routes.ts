a506616a5faa48ba62e32cb1dc226b6dbee34336
lghiggino <lghiggino@gmail.com>
2022-01-30 17:36:55 -0300

    melhoria no tratamento de erros

--- project-template-AL/src/routes/class.routes.ts
+++ project-template-AL/src/routes/class.routes.ts
@@ -5,16 +5,32 @@ import Class from '../models/Class';
 const classRouter = Router();
 
 classRouter.post('/', async (req, res) => {
-  const repo = getRepository(Class);
-  const savedClass = await repo.save(req.body);
-  return res.status(201).json(savedClass);
+  try {
+    const repo = getRepository(Class);
+    const savedClass = await repo.save(req.body);
+    return res.status(201).json(savedClass);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error?.message);
+    } else {
+      console.log(error);
+    }
+  }
 });
 
 classRouter.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const repo = getRepository(Class);
-  const classById = await repo.findOneOrFail(id);
-  return res.status(200).json(classById);
+  try {
+    const { id } = req.params;
+    const repo = getRepository(Class);
+    const classById = await repo.findOneOrFail(id);
+    return res.status(200).json(classById);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error?.message);
+    } else {
+      console.log(error);
+    }
+  }
 });
 
 export default classRouter;
