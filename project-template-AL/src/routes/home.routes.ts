39e898fc5c12bcb122d5165fbb7c25eb4c817def
lghiggino <lghiggino@gmail.com>
2022-02-07 11:11:27 -0300

    homeRouter, packageCleanUp, bad tests cleanUp

---
+++ project-template-AL/src/routes/home.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+// import { getCustomRepository, getRepository } from 'typeorm';
+// import Class from '../models/Class';
+// import ClassRepository from '../repositories/ClassRepository';
+
+const homeRouter = Router();
+
+// homeRouter.post('/', async (req, res) => {
+//   try {
+//     const repo = getRepository(Class);
+//     const savedClass = await repo.save(req.body);
+//     return res.status(201).json(savedClass);
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       console.error('error >>', error?.message);
+//     } else {
+//       console.log('error >>', error);
+//     }
+//   }
+// });
+
+homeRouter.get('/', async (req, res) => {
+  try {
+    // const { id } = req.params;
+    // const repo = getRepository(Class);
+    // const classById = await repo.findOneOrFail(id);
+    return res.status(200).json('Hello from home');
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('error >>', error?.message);
+    } else {
+      console.log('error >>', error);
+    }
+  }
+});
+
+export default homeRouter;
--- project-template-AL/src/routes/index.ts
+++ project-template-AL/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import homeRouter from './home.routes';
 import classRouter from './class.routes';
 import contentRouter from './content.routes';
 
 const routes = Router();
+
+routes.use('/', homeRouter);
 
 routes.use('/class', classRouter);
 routes.use('/content', contentRouter);
