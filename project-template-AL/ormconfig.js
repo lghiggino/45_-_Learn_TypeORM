e96c88d8b67238a8ca88ae912b1accd19036c77c
lghiggino <lghiggino@gmail.com>
2022-02-08 21:06:52 -0300

    dotenv + ormcofig.js

--- project-template-AL/ormconfig.js
+++ project-template-AL/ormconfig.js
@@ -1,25 +1,24 @@
-const port = process.env.PORT || 3306;
-
 module.exports = {
-    "type": "mysql",
-    "host": "us-cdbr-east-05.cleardb.net",
-    "port": port,
-    "username": "bcc433020b1647",
-    "password": "744ee2b0",
-    "database": "heroku_3321c3cb0c73cf1",
-    "synchronize": false,
-    "logging": false,
-    "entities": [
-      "src/models/**/*.ts",
-      "src/models/**/*.js"
+  "type": "mysql",
+  "url": process.env.CLEARDB_DATABASE_URL || process.env.DATABASE_URL,
+  "host": "localhost",
+  "port": process.env.PORT || 3306,
+  "username": "root",
+  "password": "my-secret-pw",
+  "database": "withmysql",
+  "synchronize": false,
+  "logging": false,
+  "entities": [
+    "src/models/**/*.ts",
+    "src/models/**/*.js"
+  ],
+  "migrations": [
+    "src/database/migrations/**/*.ts"
+  ],
+  "cli": {
+    "migrationsDir": [
+      "src/databse/migrations/"
     ],
-    "migrations": [
-      "src/database/migrations/**/*.ts"
-    ],
-    "cli": {
-      "migrationsDir": [
-        "src/databse/migrations/"
-      ],
-      "entitiesDir": "src/models"
-    }
+    "entitiesDir": "src/models"
   }
+}
