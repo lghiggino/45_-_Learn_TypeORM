ba145fb20f9998161029f90da96ef277d45b68d8
lghiggino <lghiggino@gmail.com>
2022-02-14 21:04:01 -0300

    createClass teste 2

---
+++ mussum/src/database/migrations/1644879754254-createClass.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateClass1644879754254 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'class',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'duration',
+            type: 'integer',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_At',
+            type: 'timestamp',
+            isNullable: true,
+            default: 'null',
+          },
+          {
+            name: 'deleted_At',
+            type: 'timestamp',
+            isNullable: true,
+            default: 'null',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('class');
+  }
+
+}
