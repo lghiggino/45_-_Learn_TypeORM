159e7bd7d218c18cefdf96e7d7489ce1b19f0c90
lghiggino <lghiggino@gmail.com>
2022-04-20 07:16:03 -0300

    rest client test + snyk first commit

--- typeOrmExpressStarter/src/routes/user.routes.ts
+++ typeOrmExpressStarter/src/routes/user.routes.ts
@@ -6,38 +6,62 @@ import myDataSource from '../app-data-source';
 const userRouter = Router();
 
 userRouter.get("/", async function (req: Request, res: Response) {
-    const users = await myDataSource.getRepository(User).find()
-    res.json(users)
+    try {
+        const users = await myDataSource.getRepository(User).find()
+        res.json(users)
+    } catch (error) {
+        res.json(error)
+    }
+
 })
 
 userRouter.get("/:id", async function (req: Request, res: Response) {
-    const results = await myDataSource.getRepository(User).findOneBy({
-        id: +req.params.id,
-    })
-    return res.send(results)
+    try {
+        const results = await myDataSource.getRepository(User).findOneBy({
+            id: +req.params.id,
+        })
+        return res.send(results)
+    } catch (error) {
+        res.json(error)
+    }
+
 })
 
 userRouter.post("/", async function (req: Request, res: Response) {
-    const user = await myDataSource.getRepository(User).create(req.body)
-    const results = await myDataSource.getRepository(User).save(user)
-    return res.send(results)
+    try {
+        const user = await myDataSource.getRepository(User).create(req.body)
+        const results = await myDataSource.getRepository(User).save(user)
+        return res.send(results)
+    } catch (error) {
+        res.json(error)
+    }
+
 })
 
 userRouter.put("/:id", async function (req: Request, res: Response) {
-    const user = await myDataSource.getRepository(User).findOneBy({
-        id: +req.params.id,
-    })
-    if (!user) {
-        return res.json("user can not be null")
+    try {
+        const user = await myDataSource.getRepository(User).findOneBy({
+            id: +req.params.id,
+        })
+        if (!user) {
+            return res.json("user can not be null")
+        }
+        myDataSource.getRepository(User).merge(user, req.body)
+        const results = await myDataSource.getRepository(User).save(user)
+        return res.send(results)
+    } catch (error) {
+        res.json(error)
     }
-    myDataSource.getRepository(User).merge(user, req.body)
-    const results = await myDataSource.getRepository(User).save(user)
-    return res.send(results)
 })
 
 userRouter.delete("/:id", async function (req: Request, res: Response) {
-    const results = await myDataSource.getRepository(User).delete(req.params.id)
-    return res.send(results)
+    try {
+        await myDataSource.getRepository(User).delete(req.params.id)
+        return res.json(`Sucessfully deleted user ${req.params.id}`)
+    } catch (error) {
+        return res.json(error)
+    }
+
 })
 
 export default userRouter;
